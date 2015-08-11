8ae8a9426dec8d6f7e2220fdfc1b9a8c06d31987
keller35 <524495217@qq.com>
2015-08-11 19:14:08 +0800

    发红包接口

--- src/fahongbao/service/qianghongbao.js
+++ src/fahongbao/service/qianghongbao.js
@@ -4,15 +4,17 @@ var uuid = require('node-uuid');
 var dbHelper = require(FRAMEWORKPATH + "/utils/dbHelper");
 
 var app_id = "wxb931d3d24994df52";
-var app_secret = "06981df67deff478460cbf396b21f016";
+var app_secret = "Yang198609Los1933ezsd230926huang";
 
 exports.route = route;
 exports.getMoney = getMoney;
+exports.getInfo = getInfo;
 
 function route(req, res, next){
 
     var state = req.query["state"];
     var code = req.query["code"];
+    var originId = req.query["originId"];
 
     // 非微信OAuth跳转
     if(!code){
@@ -33,9 +35,59 @@ function route(req, res, next){
             return;
         }
 
-        var red_url = "http://wx.naildaka.com/fahongbao/share.html?open_id=" + open_id
-            + "&nickname=" + nickname + "&headImg=" + headimgurl;
-        res.redirect(red_url);
+        var count;
+        async.series([_queryIsExist, _recordUser], function(err){
+            if(err){
+                return next(err);
+            }
+            if(originId){
+                var red_url = "http://wx.naildaka.com/fahongbao/want.html?openId=" + originId;
+                res.redirect(red_url);
+            }else {
+                var red_url = "http://wx.naildaka.com/fahongbao/share.html?openId=" + open_id;
+                res.redirect(red_url);
+            }
+        });
+
+        //查询是否存在此用户
+        function _queryIsExist(nextStep){
+            var sel_sql = "select count(1) 'count' from users where openId = :openId";
+            dbHelper.execSql(sel_sql, {openId: open_id}, function(err, result){
+                if(err){
+                    nextStep(err);
+                    return;
+                }
+                count = result[0].count;
+                nextStep();
+            });
+        }
+        //记录用户信息
+        function _recordUser(nextStep){
+            if(count > 0){
+                //存在，更新用户信息
+                var sql = "update users set nickname = :nickname,headImg = :headImg where openId = :openId";
+                var params = {nickname: nickname,headImg: headimgurl, openId: open_id};
+                dbHelper.execSql(sql, params, function(err){
+                    if(err){
+                        nextStep(err);
+                        return;
+                    }
+                    nextStep();
+                });
+            }else{
+                //不存在，存储用户信息
+                var sql = "insert into users(openId,originId,nickname,headImg,createDate) values(:openId,:originId,:nickname,:headImg,:createDate)";
+                var params = {openId: open_id,originId: originId,nickname: nickname,headImg: headimgurl, createDate: new Date().getTime()};
+                dbHelper.execSql(sql, params, function(err){
+                    if(err){
+                        nextStep(err);
+                        return;
+                    }
+                    nextStep();
+                });
+            }
+        }
+
     });
 
     function _exchangeOauth2AccessToken(callback){
@@ -74,15 +126,15 @@ function route(req, res, next){
 //获取抢红包金额
 function getMoney(req, res, next){
 
-    var open_id = req.query["open_id"];
-    if(!open_id){
-        next({errMessage: "缺少open_id"});
+    var openId = req.query["openId"];
+    if(!openId){
+        next({errMessage: "缺少openId"});
         return;
     }
 
     //查询是否抢过红包
     var sel_sql = "select count(1) 'count' from hb_records where openId = :openId";
-    dbHelper.execSql(sql, {openId: open_id}, function(err, result) {
+    dbHelper.execSql(sel_sql, {openId: openId}, function(err, result) {
         if (err) {
             next(err);
             return;
@@ -95,45 +147,83 @@ function getMoney(req, res, next){
         }
         //未抢过红包
         //生成1至100的随机数
-        var money = parseInt(Math.random() * 100);
+        var money = parseInt(Math.random() * 100) + 100;
         var id = uuid.v1();
         //记录此次抢红包
         var ins_sql = "insert into hb_records(id, openId, money, date) values(:id, :openId, :money)";
-        dbHelper.execSql(sql, {id: id,openId: open_id,money: money,date: new Date().getTime()}, function (err) {
+        dbHelper.execSql(ins_sql, {id: id,openId: openId,money: money,date: new Date().getTime()}, function (err) {
             if (err) {
                 next(err);
                 return;
             }
             doResponse(req, res, {money: money});
+
             //异步调用发红包接口
-            var params = {
-                mch_id: "1255492301",
-                wxappid: "wxb931d3d24994df52",
-                nick_name: "美甲大咖",
-                send_name: "美甲大咖",
-                re_openid: "oZWB6wVxwZBN17OpjsxoV_UX-b8Y",
-                total_amount: 100,
-                wishing: "测试发送红包",
-                act_name: "美甲大咖送红包活动",
-                remark: "快来领啊",
-                logo_imgurl: "http://ypicture.oss-cn-hangzhou.aliyuncs.com/120.png"
-            };
-
-            api.cash_redpack(params, "Yang198609Los1933ezsd230926huang", "/Users/apple/git_local/wechat-toolkit/cer/apiclient_cert.p12", function(err, code, result){
-
-                if(err){
-                    console.log(err);
-                    return;
-                }
-
-                if(code === 0){
-                    console.log("调用成功");
-                }else{
-                    console.log("调用失败");
-                }
-
-                console.log(result);
-            });
+            sendRedPack(openId, money);
+            //同时为originId发红包
+            sendRedPackToOrigin(openId, money);
         });
+    });
+}
+
+//查询用户信息
+function getInfo(req, res, next){
+    var openId = req.params["openId"];
+    if(!openId){
+        res.send("缺少openId");
+        return;
+    }
+    var sql = "select nickname,headImg from users where openId = :openId";
+    dbHelper.execSql(sql, {openId: openId}, function(err, result){
+        if(err){
+            next(err);
+            return;
+        }
+        doResponse(req, res, result[0]);
+    });
+}
+
+function sendRedPackToOrigin(openId, money){
+    //根据openId查询originId
+    var sql = "select originId from users where openId = :openId";
+    dbHelper.execSql(sql, {openId: openId}, function(err, result){
+        if(err){
+            next(err);
+            return;
+        }
+        //存在分享来源用户
+        if(result[0].originId){
+            sendRedPack(result[0].originId, money);
+        }
+    });
+}
+
+function sendRedPack(openId, money) {
+    var params = {
+        mch_id: "1255492301",
+        wxappid: app_id,
+        nick_name: "美甲大咖",
+        send_name: "美甲大咖",
+        re_openid: openId,
+        total_amount: money,
+        wishing: "恭喜发财，大吉大利!",
+        act_name: "美甲大咖送红包活动",
+        remark: "快来领啊",
+        logo_imgurl: "http://ypicture.oss-cn-hangzhou.aliyuncs.com/120.png"
+    };
+    api.cash_redpack(params, app_secret, "/Users/apple/git_local/wechat-toolkit/cer/apiclient_cert.p12", function (err, code, result) {
+
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        if (code === 0) {
+            console.log("调用成功");
+        } else {
+            console.log("调用失败");
+        }
+
+        console.log(result);
     });
 }

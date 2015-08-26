17db14a5cf8e1b25e2c8e649faa358ff109fcc30
baisusu <yuan404kang@126.com>
2015-08-26 15:17:20 +0800

    获取点赞数

--- src/interview/static/js/index.js
+++ src/interview/static/js/index.js
@@ -1,78 +1,62 @@
 Zepto(function($){
-	var teacher = [
-		{
-			name:'胡波',
-			img:'images/1.png'
-		},
-		{
-			name:'董亚坡',
-			img:'images/2.png'
-		},
-		{
-			name:'王红',
-			img:'images/3.png'
-		},
-		{
-			name:'郭东',
-			img:'images/4.png'
-		},
-		{
-			name:'胡小',
-			img:'images/5.png'
-		},
-		{
-			name:'杨帆',
-			img:'images/6.png'
-		},
-		{
-			name:'李智',
-			img:'images/7.png'
-		},
-		{
-			name:'薛兴亚',
-			img:'images/8.png'
-		},
-		{
-			name:'汤志平',
-			img:'images/9.png'
-		},
-		{
-			name:'肖莎',
-			img:'images/10.png'
-		},
-		{
-			name:'媛媛',
-			img:'images/11.png'
-		}
-	];
-	var modal = false;
-	var boxsVue;
-	function creatComponent () {
-		Vue.component('boxs-template',{
-			template:'#boxs-template'
-		})
-	}
-	creatComponent();
-	function creatVue () {
-		boxsVue = new Vue({
-			el:'#boxList',
-			data:{
-				teacher:teacher,
-				modal:modal
-			},
-			methods:{
-				modalShow:function () {
-					boxsVue.$data.modal = false;
-				}
-			}
-		})
-	}
-	creatVue();
 	function ceratStorage () {
 		if(!window.localStorage.getItem('modal')){
-			boxsVue.$data.modal = true;
+			$('.modal').show();
 			window.localStorage.setItem('modal',true);
 		}
 	}
 	ceratStorage ();
+	$('.modal').click(function() {
+		$('.modal').hide();
+	});
+
+	function getLikedNum () {
+		 getRequest('http://huodong.naildaka.com/svc/interview/getAllLikeCount', function(error,data) {
+			if (data.code !=0) {
+				callback(null,data);
+				return;
+			}
+			console.log(data.result);
+			var boxs = $('.likedNum');
+			$.each(data.result.counts, function(i) {
+				$('#t'+data.result.counts[i].t).text(data.result.counts[i].count);	
+			});
+		});
+	}
+	getLikedNum ();
+	function count (useragent) {
+		var ua = window.navigator.userAgent.toLowerCase();
+		if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+			var useragent = 1;
+			var url = 'http://huodong.naildaka.com/svc/interview/count?useragent=1';
+		}else{
+			var useragent = 2;
+			var url = 'http://huodong.naildaka.com/svc/interview/count?useragent=2';
+		}
+		getRequest(url,useragent,function (error,data) {
+			if (data.code !=0) {
+				callback(null,data);
+				return;
+			}
+			callback(null,data);
+		})
+	}
+	count();
+	//封装get请求
+	function getRequest(url,callback) {
+		$.ajax({
+			type: 'GET',
+			url: url,
+			beforeSend: function(request) {
+	                    
+	        },
+			success:function(data){
+				callback(null,data);
+			},
+			error: function(error){
+				console.log(error);
+			},
+			dataType:"json"
+		});
+	}
 });

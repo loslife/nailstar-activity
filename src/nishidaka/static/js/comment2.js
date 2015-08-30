7460103020f307fb09dd5ce5cb5851ad5cf8a6d3
室长大人 <1240429156@qq.com>
2015-08-30 23:01:26 +0800

    合并CSS文件

--- src/nishidaka/static/js/comment2.js
+++ src/nishidaka/static/js/comment2.js
@@ -3,6 +3,86 @@ Zepto(function($){
         ev.preventDefault();
     });
 
+    var commentHtml = {
+        commentVue:'',
+        commentView:{},
+        items:{
+            item1:{
+                commentTitle:"1.适合此款美甲的场所:",
+                optionA:"婚礼",
+                optionB:"闺蜜趴",
+                optionC:"烛光晚餐"
+            },
+            item2:{
+                commentTitle:"2.适合此款美甲的场所:",
+                optionA:"婚礼",
+                optionB:"闺蜜趴",
+                optionC:"烛光晚餐"
+            },
+            item3:{
+                commentTitle:"3.适合此款美甲的场所:",
+                optionA:"婚礼",
+                optionB:"闺蜜趴",
+                optionC:"烛光晚餐"
+            },
+            item4:{
+                commentTitle:"4.适合此款美甲的场所:",
+                optionA:"婚礼",
+                optionB:"闺蜜趴",
+                optionC:"烛光晚餐"
+            },
+            item5:{
+                commentTitle:"5.适合此款美甲的场所:",
+                optionA:"婚礼",
+                optionB:"闺蜜趴",
+                optionC:"烛光晚餐"
+            },
+            item6:{
+                commentTitle:"6.适合此款美甲的场所:",
+                optionA:"婚礼",
+                optionB:"闺蜜趴",
+                optionC:"烛光晚餐"
+            },
+        }
+    }
+
+    //绑定模型
+    function creatVue(){
+        commentHtml.commentVue = new Vue({
+            el:'#comentHtml',
+            data:{
+
+            },
+            methods:{
+                choose:function(){
+                    $(this).addClass('collected');
+                    var val = $(this).attr("val");
+                    var itemIndex = $(this).parent('div').parent().parent().attr('index');
+
+                    //点亮指示灯
+                    $('.origin-div').children().eq(itemIndex).addClass('origin-light').siblings().removeClass('origin-light');
+
+                    //跳转到下一页
+                    if (swich<8) {
+                        $(boxs[swich+1]).addClass('hide_small');
+                        $(boxs[swich+2]).removeClass('hide');
+                        $(boxs[swich+2]).removeClass('show_big');
+                        $(boxs[swich+2]).addClass('show');
+                        $('.comment-div').eq(itemIndex).addClass('slideDown');
+                        swich++;
+                        if(swich==6 ){
+                            $('.origin-div').hide();
+                        }
+                    }
+
+                    //显示统计的分数
+                    var mark = $('.top-mark');
+                    mark.text(sub+40);
+                }
+            }
+        })
+    }
+
     var result = [0, 0, 0, 0, 2, 2];
     var boxs = $('.comment .item');
     var swich = 0;
@@ -17,87 +97,6 @@ Zepto(function($){
         $(boxs[1]).addClass('show');
         $('.origin-div').show(1500);
         $('.comment-div-first').addClass('slideDown')
-    })
-
-    option.on('tap',function(){
-        $(this).addClass('collected');
-        var val = $(this).attr("val");
-        var itemIndex = $(this).parent('div').parent().parent().attr('index');
-
-        //统计每道题选择之后的分数
-        if(itemIndex == 1){
-            var num = parseInt(10*Math.random());
-            if($(this).attr('val')== result[0]){
-                sub += 10;
-            }else{
-                sub += num;
-            }
-        }
-
-        if(itemIndex == 2){
-            var num = parseInt(10*Math.random());
-            if($(this).attr('val')== result[1]){
-                sub += 10;
-            }else{
-                sub += num;
-            }
-        }
-
-        if(itemIndex == 3){
-            var num = parseInt(10*Math.random());
-            if($(this).attr('val')== result[2]){
-                sub += 10;
-            }else{
-                sub += num;
-            }
-        }
-
-        if(itemIndex == 4){
-            var num = parseInt(10*Math.random());
-            if($(this).attr('val')== result[3]){
-                sub += 10;
-            }else{
-                sub += num;
-            }
-        }
-
-        if(itemIndex == 5){
-            var num = parseInt(10*Math.random());
-            if($(this).attr('val')== result[4]){
-                sub += 10;
-            }else{
-                sub += num;
-            }
-        }
-
-        if(itemIndex == 6){
-            var num = parseInt(10*Math.random());
-            if($(this).attr('val')== result[5]){
-                sub += 10;
-            }else{
-                sub += num;
-            }
-        }
-
-        //点亮指示灯
-        $('.origin-div').children().eq(itemIndex).addClass('origin-light').siblings().removeClass('origin-light');
-
-        //跳转到下一页
-        if (swich<8) {
-            $(boxs[swich+1]).addClass('hide_small');
-            $(boxs[swich+2]).removeClass('hide');
-            $(boxs[swich+2]).removeClass('show_big');
-            $(boxs[swich+2]).addClass('show');
-            $('.comment-div').eq(itemIndex).addClass('slideDown');
-            swich++;
-            if(swich==6 ){
-                $('.origin-div').hide();
-            }
-        }
-
-        //显示统计的分数
-        var mark = $('.top-mark');
-        mark.text(sub+40);
     })
 
     //分享时显示的遮罩层

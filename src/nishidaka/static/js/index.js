432103512c7af93cb823cabf21d9df48ac0c8a59
小龙龙 <1240429156@qq.com>
2015-09-10 17:14:58 +0800

    全部答对时分享情况

--- src/nishidaka/static/js/index.js
+++ src/nishidaka/static/js/index.js
@@ -142,10 +142,17 @@ Zepto(function($){
         }
 
         //显示答对的个数
-        percent.text(score);
+        var str;
+        if(score === 6){
+            $('.top-count').text('哇晒！美甲大咖原来是你!');
+            str = "";
+        }else{
+            percent.text(score);
+            str = "我认出了" + score + "个,不服来战。";
+        }
 
         //配置好友分享
-        shareWX(score)();
+        shareWX(str)();
     });
 
     //分享时显示的遮罩层
@@ -197,7 +204,8 @@ Zepto(function($){
                     ]
                 });
                 //微信分享
-                wx.ready(shareWX(0));
+                var str = "'我认出了0个,不服来战。'";
+                wx.ready(shareWX(str));
             },
             error: function(err){
                 console.log(err);
@@ -206,11 +214,11 @@ Zepto(function($){
         });
     }
 
-    function shareWX(score){
+    function shareWX(str){
         return function(){
             wx.onMenuShareAppMessage({
                 title: '最新的美甲产品和款式,你认识吗?', // 分享标题
-                desc: '我认出了' + score + '个,不服来战。', // 分享描述
+                desc: str, // 分享描述
                 link: 'http://huodong.naildaka.com/nishidaka/index.html', // 分享链接
                 imgUrl: 'http://huodong.naildaka.com/nishidaka/images/index/share_icon.png', // 分享图标
                 success: function () {
@@ -224,7 +232,7 @@ Zepto(function($){
             wx.onMenuShareTimeline({
                 title: '最新的美甲产品于款式你了解多少?',
                 link: 'http://huodong.naildaka.com/nishidaka/index.html',
-                imgUrl: 'hhttp://huodong.naildaka.com/nishidaka/images/index/share_icon.png',
+                imgUrl: 'http://huodong.naildaka.com/nishidaka/images/index/share_icon.png',
                 success: function () {
                     transparency.hide();
                 },

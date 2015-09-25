f0eb15c02509a9a820afae138a1922edfafc806f
小龙龙 <1240429156@qq.com>
2015-09-25 19:42:00 +0800

    friend页面

--- src/zhongqiu/gulp_zhongqiu.js
+++ src/zhongqiu/gulp_zhongqiu.js
@@ -11,10 +11,35 @@ gulp.task('me_js', function () {
         .pipe(rename('me.min.js'))
         .pipe(gulp.dest('./src/zhongqiu/static/js'));
 });
-
 gulp.task('me_css', function () {
     return gulp.src(['./src/zhongqiu/static/css/me.css'])
         .pipe(minifyCss())
         .pipe(rename('me.min.css'))
+        .pipe(gulp.dest('./src/zhongqiu/static/css'));
+});
+
+gulp.task('introduce_js', function () {
+    return gulp.src('./src/zhongqiu/static/js/introduce.js')
+        .pipe(uglify())
+        .pipe(rename('introduce.min.js'))
+        .pipe(gulp.dest('./src/zhongqiu/static/js'));
+});
+gulp.task('introduce_css', function () {
+    return gulp.src(['./src/zhongqiu/static/css/introduce.css'])
+        .pipe(minifyCss())
+        .pipe(rename('introduce.min.css'))
+        .pipe(gulp.dest('./src/zhongqiu/static/css'));
+});
+
+gulp.task('friend_js', function () {
+    return gulp.src('./src/zhongqiu/static/js/friend.js')
+        .pipe(uglify())
+        .pipe(rename('friend.min.js'))
+        .pipe(gulp.dest('./src/zhongqiu/static/js'));
+});
+gulp.task('friend_css', function () {
+    return gulp.src(['./src/zhongqiu/static/css/friend.css'])
+        .pipe(minifyCss())
+        .pipe(rename('friend.min.css'))
         .pipe(gulp.dest('./src/zhongqiu/static/css'));
 });
